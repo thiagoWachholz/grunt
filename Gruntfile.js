cf9508633c2fa9d23bd7be86d313d7691414b5af
Thiago Bierhals Wachholz <thiagowachholz330@gmail.com>
2024-06-11 20:36:42 -0300

    3

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,11 +54,11 @@ module.exports = function(grunt) {
                     patterns: [
                         {
                             match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
+                            replacement: '../styles/main.min.css'
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
+                            replacement: '../scripts/main.min.js'
                         }
                     ]
                 },
